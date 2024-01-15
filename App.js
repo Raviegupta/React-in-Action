72bea417b3e4c64029877399c1568a2cfbf0c9e4
Raviegupta <raviegupta153@gmail.com>
2024-01-15 20:26:47 +0530

    practice: creating elemnnts using react cdn

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-const heading = React.createElement('h1', {id:'heading'}, 'LIST OF FRUITS');
+// const heading = React.createElement("h1", { id: "heading" }, "LIST OF FRUITS");
 
-const li1 = React.createElement('li', {}, 'Apple');
-const li2 = React.createElement('li', {}, 'Mango');
-const li3 = React.createElement('li', {}, 'Banana');
-const ul = React.createElement('ul', {class:'lists'}, [li1, li2, li3]);
+// const li1 = React.createElement('li', {}, 'Apple');
+// const li2 = React.createElement('li', {}, 'Mango');
+// const li3 = React.createElement('li', {}, 'Banana');
+// const ul = React.createElement(
+//   "ul",
+//   {
+//     class: "lists",
+//   },
+//   [
+//     React.createElement("li", {}, "Apple"),
+//     React.createElement("li", {}, "Mango"),
+//     React.createElement("li", {}, "Banana"),
+//   ]
+// );
 
-const container = React.createElement('div', {id: 'container'}, [heading, ul]);
-console.log(container)
+const container = React.createElement("div", { id: "container" }, [
+  React.createElement("h1", { id: "heading" }, "LIST OF FRUITS"),
+  React.createElement(
+    "ul",
+    {
+      className: "lists",
+    },
+    [
+      React.createElement("li", { key: 1 }, "Apple"),
+      React.createElement("li", { key: 2 }, "Mango"),
+      React.createElement("li", { key: 3 }, "Banana"),
+    ]
+  ),
+]);
+console.log(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
