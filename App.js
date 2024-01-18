f5ff71e55e40d4a04ec0fc494374bf5488904c39
Raviegupta <raviegupta153@gmail.com>
2024-01-18 10:46:16 +0530

    implementation of routing

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/Components/Header";
 import Body from "./src/Components/Body";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./src/Pages/About";
+import Contact from "./src/Pages/Contact";
+import Error from "./src/Pages/Error";
 
 
 const AppLayout = () => {
@@ -13,5 +17,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    "path": "/",
+    "element": <AppLayout />,
+    "errorElement": <Error />
+  },
+  {
+    "path": "/about",
+    "element": <About />
+  },
+  {
+    "path": "/contact",
+    "element": <Contact />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(AppLayout());
+root.render(<RouterProvider router={appRouter}/>);
