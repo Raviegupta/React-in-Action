d00ef153680a77da1a144ae2d918d91b0d376b3f
Raviegupta <raviegupta153@gmail.com>
2024-01-31 17:43:14 +0530

    corrections

--- src/Pages/RestaurantMenu.jsx
+++ src/Pages/RestaurantMenu.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
 import Shimmer from "../Components/Shimmer";
 
